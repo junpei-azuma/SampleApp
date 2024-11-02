7a85f364df13f48e674ecc655fe90d39495c6eae
junpei.azuma <azumanotdetail@gmail.com>
2024-11-02 22:45:00 +0900

    First Commit

---
+++ src/main/kotlin/com/example/sampleapp/SampleAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sampleapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleAppApplication>(*args)
+}
